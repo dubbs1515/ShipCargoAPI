558fed8357d4748c25dd735e96a80714b8831eb5
Christopher Dubbs <christopherdubbs@Christophers-MacBook-Pro.local>
2018-10-23 20:52:55 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ Author: Christopher Dubbs
+ API Name: Cargo Ship API
+ Date Last Modified: October 27, 2018
+ Last Modified By: Christopher Dubbs
+ Hosting: Google Cloud AppEngine and Google Cloud Datastore
+*******************************************************************************/
+const express = require ('express');
+const app = express();
+const Datastore = require('@google-cloud/datastore');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5015;
+var path = require("path");
+app.use(express.static(__dirname));
+
+const projectId = 'ship-cargo-api-dubbsc';
+const datastore = new Datastore({
+	projectId: projectId,
+});
+
+// Express Routing
+const router = express.Router();
+
+// Express Middleware
+app.use(bodyParser.json());
+
+// API Datastore Kinds (akin to RDB tables)
+const SHIP = "Ship"; 
+const CARGO = "Cargo";
+
+const ROOT_URL ="localhost:5015/"
+// const ROOT_URL = "http://ship-cargo-api-dubbsc.appspot.com";
+
+
+
+/*******************************************************************************
+ * HELPER FUNCTIONS
+ ******************************************************************************/
+ /******************************************************************************
+ * Name: fromDataStore
+ * Description: Helper function for datastore interaction. A key is stored 
+ ******************************************************************************/
+function fromDataStore(item) {
+	item.id = item[Datastore.KEY].id;
+	return item;	
+}
+
+/*******************************************************************************
+ * MODEL FUNCTIONS  (Used to interact with datastore)
+ ******************************************************************************/
+
+ /******************************************************************************
+ * Name: get_entities
+ * Description: Returns the entities held in the datastore, as specified by the
+ *  entity "kind" argument (i.e. SHIP or CARGO)
+ ******************************************************************************/
+function get_entities(kind) {
+    const q = datastore.createQuery(kind); // Create Query
+    // Run Query
+    return datastore.runQuery(q).then((entities) => {
+        return entities[0].map(fromDataStore);
+    });
+}
+
+
+/*******************************************************************************
+ * Name: get_entity
+ * Description: Returns the entity held in the datastore, as specified by the
+ *  entity "kind" argument (i.e. SHIP or CARGO) and the id argument
+ ******************************************************************************/
+function get_entity(kind, id) {
+    let key = datastore.key([kind, parseInt(id, 10)]);
+    const q = datastore.createQuery(kind).filter('__key__', '=', key); 
+    return datastore.runQuery(q).then((entity) => {                 
+        return entity[0].map(fromDataStore);
+    });
+
+}
+
+
+/*******************************************************************************
+ * CONTROLLER FUNCTIONS (Handle routing)
+ ******************************************************************************/
+
+/*******************************************************************************
+ * END OF CONTROLLER FUNCTIONS
+ ******************************************************************************/
+
+app.use(express.static("public"));
+app.use('', router);
+
+// Listen on App-Engine Port or 5015
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}... :)`);
+});
+
+
